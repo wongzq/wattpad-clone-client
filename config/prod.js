be6bbe0ef6274a0f6e3f9ac4e977ea7289bb2326
WONG ZHENG QUAN <38060298+wongzhengquan@users.noreply.github.com>
2020-08-31 17:53:24 +0800

    Setup

---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGODB_URI: process.env.MONGODB_URI,
+  JWT_SECRET: process.env.JWT_SECRET
+}
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const { MONGODB_URI } = require('./config/keys')
+const PORT = process.env.PORT || 5000
+const app = express()
+
+// connect to MongoDB
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+}).catch(err => console.log(err))
+mongoose.connection.on('connected', () => console.log(`MongoDB connection success`))
+mongoose.connection.on('error', (err) => console.log(`MongoDB connection error:\n${err}`))
+
+// routes
+app.use(cors())
+app.use(express.json())
+
+// listen
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
